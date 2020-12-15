615a54d575cd81b42fe2b9d7a764978dc3625df0
Brendon Costa <brendoncosta93@gmail.com>
2020-12-15 14:44:11 -0300

    create new project

--- Shop/Startup.cs
+++ Shop/Startup.cs
@@ -1,8 +1,15 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+
+namespace Shop
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
--- Shop/models/Category.cs
+++ Shop/models/Category.cs
@@ -1,15 +1,16 @@
+
 using System.ComponentModel.DataAnnotations;
 
 namespace Shop.Models
 {
-    public class Category 
+    public class Category
     {
         [Key]
-        public int Id {get; set;}
+        public int Id { get; set;}
 
         [Required(ErrorMessage = "Este campo é obrigatório")]
         [MaxLength(60, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
         [MinLength(3, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
-        public string Title {get; set;}
+        public string Title { get; set;}
     }
 }
